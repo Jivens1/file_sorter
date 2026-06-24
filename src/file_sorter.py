f46ea8c61481f4f7d2fc4c541d386aece79f1e42
Jivens1 <jivensishimwe1@gmail.com>
2026-06-24 18:33:21 +0200

    Update comments for clarity in file_sorter.py

--- src/file_sorter.py
+++ src/file_sorter.py
@@ -1,26 +1,26 @@
 import os
 import shutil
 
-# 1. WHERE to look
+# WHERE to look
 folder_path = r"C:\PAITOON\file_sorter\test"
 
-# 2. CREATE the destination folders (if they don't exist)
+# CREATE the destination folders (if they don't exist)
 os.makedirs(os.path.join(folder_path, "Images"), exist_ok=True)
 os.makedirs(os.path.join(folder_path, "Documents"), exist_ok=True)
 os.makedirs(os.path.join(folder_path, "Music"), exist_ok=True)
 os.makedirs(os.path.join(folder_path, "Presentations"), exist_ok=True)
 
-# 3. GET all items in the folder
+# GET all items in the folder
 all_items = os.listdir(folder_path)
 
-# 4. LOOP through each item
+
 for item in all_items:
-    # 5. SKIP if it's a folder (we don't want to move folders!)
+    #  SKIP if it's a folder
     full_path = os.path.join(folder_path, item)
     if os.path.isdir(full_path):
         continue
     
-    # 6. CHECK the file type and MOVE it
+    #  CHECK the file type and MOVE it
     if item.endswith((".jpg", ".PNG", ".png", ".gif")):
         destination = os.path.join(folder_path, "Images", item)
         shutil.move(full_path, destination)
